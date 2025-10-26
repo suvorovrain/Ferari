96848debf77b41c3f3642d96ff5338ffb547dfe5
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-27 01:35:53 +0300

    fix: add  module into main
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- src/input.rs
+++ src/input.rs
@@ -8,7 +8,10 @@ pub struct Input {
 }
 
 impl Input {
-    pub fn update(&mut self, window: Window) {
+    pub fn new() -> Self {
+        Self { up: false, left: false, down: false, right: false }
+    }
+    pub fn update(&mut self, window: &Window) {
         self.up = window.is_key_down(Key::W);
         self.left = window.is_key_down(Key::A);
         self.down = window.is_key_down(Key::S);
