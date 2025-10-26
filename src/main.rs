b6e05cc02c2b54e5eeb902f5adb7a09751f0e204
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-10-26 15:22:17 +0300

    refactor: apply formatter
    Signed-off-by: Rodion Suvorov <rodion.suvorov.94@mail.ru>

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,12 @@ const HEIGHT: usize = 360;
 fn main() {
     let result = get_five();
     println!("Function returned: {}", result);
-      let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    let mut window = Window::new(
-        "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+    let mut window = Window::new("Test - ESC to exit", WIDTH, HEIGHT, WindowOptions::default())
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
 
     // Limit to max ~60 fps update rate
     window.set_target_fps(60);
@@ -28,8 +23,6 @@ fn main() {
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
-            .unwrap();
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
     }
 }
