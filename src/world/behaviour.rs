912e1b3fced8bfcee5a91b8d8d788217aa2049c8
homka122 <kombaev02@gmail.com>
2025-10-28 13:15:48 +0300

    Feat: add animations and collisions

--- src/world/behaviour.rs
+++ src/world/behaviour.rs
@@ -1,9 +1,17 @@
+use std::vec;
+
 use crate::input::InputSnapshot;
 
 use super::State;
 
-fn normalize_vector(dx: f32, dy: f32) -> (f32, f32) {
-    let distance = (dx * dx + dy * dy).sqrt();
+fn abs_vector(vec: (f32, f32)) -> f32 {
+    let (dx, dy) = vec;
+    return (dx * dx + dy * dy).sqrt();
+}
+
+fn normalize_vector(vec: (f32, f32)) -> (f32, f32) {
+    let (dx, dy) = vec;
+    let distance = abs_vector(vec);
     if distance > 0.1 {
         (dx / distance, dy / distance)
     } else {
@@ -12,17 +20,33 @@ fn normalize_vector(dx: f32, dy: f32) -> (f32, f32) {
 }
 
 pub fn make_step(curState: &mut State, inputState: &InputSnapshot) {
-    let player = &mut curState.player;
     let player_speed = 0.75;
-    player.x += if inputState.right { player_speed } else { 0.0 };
-    player.x += if inputState.left { -player_speed } else { 0.0 };
-    player.y += if inputState.up { -player_speed } else { 0.0 };
-    player.y += if inputState.down { player_speed } else { 0.0 };
+    let collision_distance = 10.0;
+
+    let player = &mut curState.player;
+
+    let mut player_move_vec = (0.0, 0.0);
+    player_move_vec.0 += if inputState.right { 1.0 } else { 0.0 };
+    player_move_vec.0 += if inputState.left { -1.0 } else { 0.0 };
+    player_move_vec.1 += if inputState.up { -1.0 } else { 0.0 };
+    player_move_vec.1 += if inputState.down { 1.0 } else { 0.0 };
+
+    let norm = normalize_vector(player_move_vec);
+    player.x += norm.0 * player_speed;
+    player.y += norm.1 * player_speed;
 
     // make that mob go to player
     for mob in &mut curState.mobs {
         let vec_to = (player.x - mob.x, player.y - mob.y);
-        let norm = normalize_vector(vec_to.0, vec_to.1);
+        if abs_vector(vec_to) <= collision_distance {
+            let vec_from = (mob.x - player.x, mob.y - player.y);
+            let norm = normalize_vector(vec_from);
+            mob.x = player.x + norm.0 * collision_distance;
+            mob.y = player.y + norm.1 * collision_distance;
+            continue;
+        }
+        println!("{}", abs_vector(vec_to));
+        let norm = normalize_vector(vec_to);
         // length of vec_move is |speed|
         let mob_speed = (if mob.x_speed != 0. { mob.x_speed } else { mob.y_speed }).abs();
         let vec_move = (norm.0 * mob_speed, norm.1 * mob_speed);
